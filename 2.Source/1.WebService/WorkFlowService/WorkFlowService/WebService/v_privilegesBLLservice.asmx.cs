f4b841416ce93123f54e85f9e6525c69c5c5fbc1
zhjg <zhjg.1987@163.com>
2013-06-13 10:47:47 +0800

    111

--- 2.Source/1.WebService/WorkFlowService/WorkFlowService/WebService/v_privilegesBLLservice.asmx.cs
+++ 2.Source/1.WebService/WorkFlowService/WorkFlowService/WebService/v_privilegesBLLservice.asmx.cs
@@ -4,10 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Services;
-<<<<<<< HEAD
 using System.Web.Services.Protocols;
-=======
->>>>>>> df28b0b202da255851e8d8093c0ecee4aeaf9f95
 using Saron.WorkFlowService.Model;
 
 namespace Saron.WorkFlowService.WebService
@@ -25,15 +22,14 @@ namespace Saron.WorkFlowService.WebService
         private readonly Saron.WorkFlowService.DAL.v_privilegesDAL m_v_privilegesDal = new Saron.WorkFlowService.DAL.v_privilegesDAL();
         private readonly Saron.WorkFlowService.DAL.v_usersDAL m_v_userDal = new Saron.WorkFlowService.DAL.v_usersDAL();
 
-<<<<<<< HEAD
         public SecurityContext m_securityContext = new SecurityContext();
 
         [SoapHeader("m_securityContext")]
         [WebMethod(Description = "判断用户是否有访问项目的权限（userID：用户ID，item_Code：项目编码，pt_Name：权限类型名称，appID：系统ID）")]
-        public bool UserIsItemPrivilege(int userID, string item_Code, string pt_Name, int appID,out string msg)
+        public bool UserIsItemPrivilege(int userID, string item_Code, string pt_Name, int appID, out string msg)
         {
             //对webservice进行授权验证
-            if (!m_securityContext.UserIsValid(m_securityContext.UserName, m_securityContext.PassWord, m_securityContext.AppID,out msg))
+            if (!m_securityContext.UserIsValid(m_securityContext.UserName, m_securityContext.PassWord, m_securityContext.AppID, out msg))
             {
                 //webservice用户未授权，msg提示信息
                 return false;
@@ -59,21 +55,7 @@ namespace Saron.WorkFlowService.WebService
                 msg = "无权限访问此项目";
                 return false;
             }
-=======
-        /// <summary>
-        /// 判断用户是否有访问项目的权限
-        /// </summary>
-        [WebMethod(Description = "判断用户是否有访问项目的权限（userID：用户ID，item_Code：项目编码，pt_Name：权限类型名称，appID：系统ID）")]
-        public bool UserIsItemPrivilege(int userID, string item_Code, string pt_Name, int appID)
-        {
-            int privilegeID = m_v_privilegesDal.GetPrivilegeID(item_Code, pt_Name, appID);
-            if (privilegeID == 0)
-            {
-                return true;
-            }
-
-            return m_v_userDal.ExistsUser_Privilege(userID,privilegeID);
->>>>>>> df28b0b202da255851e8d8093c0ecee4aeaf9f95
         }
+
     }
 }
