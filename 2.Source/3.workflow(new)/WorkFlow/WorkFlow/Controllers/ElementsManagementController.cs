fc9d8ff4be9080b267dd667b0b9b67aef6d983f5
zhjg <zhjg.1987@163.com>
2013-06-14 12:47:13 +0800

    乱码

--- 2.Source/3.workflow(new)/WorkFlow/WorkFlow/Controllers/ElementsManagementController.cs
+++ 2.Source/3.workflow(new)/WorkFlow/WorkFlow/Controllers/ElementsManagementController.cs
@@ -252,16 +252,9 @@ namespace WorkFlow.Controllers
                     return Json(new Saron.WorkFlow.Models.InformationModel { success = false, css = "p-errorDIV", message = "已经存在相同的元素名称!" });
                 }
             }
-<<<<<<< HEAD
 
             DataSet codeds = m_elementsBllService.GetCodeListOfMenuApp(appID,menuID,out msg);
-=======
-<<<<<<< HEAD
-            DataSet codeds = m_elementsBllService.GetCodeListOfMenuApp(appID,menuID,out msg);
-=======
-            DataSet codeds = m_elementsBllService.GetCodeListOfMenuApp(appID,menuID);
->>>>>>> 52d49ddbec753b9dd743017bfa2fd9596f9c5ae7
->>>>>>> 577c5dccaf2c34a14e83f9986100eb9eed228bd8
+
             ArrayList codeList = new ArrayList();
             var codetotal = codeds.Tables[0].Rows.Count;
             for (int i = 0; i < codetotal; i++)
